d7452702851cb6d58c675b75c9905da19d99b415
elizuzurilu <125546906+elizuzurilu@users.noreply.github.com>
2023-02-21 18:55:58 -0600

    configurado el settings con los ambientes virtuales dev y prod

--- lugares_seguros/lugares_seguros/settings/base.py
+++ lugares_seguros/lugares_seguros/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&_bb05q3f7g35j_xs6xx+9b5g9srt4b9%k6js381z4(@-k@x@#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
